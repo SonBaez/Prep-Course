2a86891c2d19b6c4bb2e267d6297b7aa114e1041
Sonbaez <sonbaez81@gmail.com>
2022-10-19 20:01:58 -0300

    commit 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -178,7 +178,7 @@ function elevarAlCubo(num) {
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  return Math.pow (nu, exponent)
+  return Math.pow (num, exponent)
 }
 
 function redondearNumero(num) {
